a832d53cf1d501121fcad73c2d5b1bb663a8b7de
Yuriy Sobolyev <ura1111@gmail.com>
2024-12-11 23:19:36 +0200

    Add show more btn and filtering

--- src/component/CryptoTable.jsx
+++ src/component/CryptoTable.jsx
@@ -1,37 +1,105 @@
-import React, {useContext} from 'react';
+import React, { useState, useContext } from "react";
 import selectedCryptoContext from "../store/selectedCryptoContext.jsx";
 import cryptoListContext from "../store/cryptoListContext.jsx";
 
 function CryptoTable() {
-    const {cryptoList} = useContext(cryptoListContext);
-    const {getHistory} = useContext(selectedCryptoContext);
+    const { cryptoList } = useContext(cryptoListContext);
+    const { getHistory } = useContext(selectedCryptoContext);
+
+    // Состояние для отслеживания количества отображаемых элементов
+    const [visibleCount, setVisibleCount] = useState(10);
+
+    // Состояние для текущего критерия сортировки
+    const [sortConfig, setSortConfig] = useState({ key: "rank", direction: "asc" });
+
+    // Функция для сортировки массива
+    const sortedCryptoList = [...cryptoList].sort((a, b) => {
+        const aValue = sortConfig.key === "rank" ? parseInt(a[sortConfig.key]) : a[sortConfig.key];
+        const bValue = sortConfig.key === "rank" ? parseInt(b[sortConfig.key]) : b[sortConfig.key];
+
+        if (aValue < bValue) {
+            return sortConfig.direction === "asc" ? -1 : 1;
+        }
+        if (aValue > bValue) {
+            return sortConfig.direction === "asc" ? 1 : -1;
+        }
+        return 0;
+    });
+
+    // Функция для изменения критерия сортировки
+    const handleSort = (key) => {
+        setSortConfig((prevConfig) => ({
+            key,
+            direction: prevConfig.key === key && prevConfig.direction === "asc" ? "desc" : "asc",
+        }));
+    };
+
+    // Функция для показа больше элементов
+    const showMore = () => {
+        setVisibleCount((prevCount) => prevCount + 10);
+    };
 
     return (
-        <ul className="crypto_table">
-            <li className="crypto_table_header">
-                <span className="crypto_table_header__symbol">Coin</span>
-                <span className="crypto_table_header__price">Price</span>
-                <span className="crypto_table_header__change">24Hr %</span>
-            </li>
-            {cryptoList.slice(0, 10).map((crypto) => (
-                <li className="crypto_table_item" key={crypto.id} onClick={() => getHistory(crypto.id)}>
+        <div>
+            <ul className="crypto_table">
+                <li className="crypto_table_header">
+                    <span className="crypto_table_header__rank" onClick={() => handleSort("rank")}>
+                        # {sortConfig.key === "rank" && (sortConfig.direction === "asc" ? "↑" : "↓")}
+                    </span>
+                    <span className="crypto_table_header__symbol" onClick={() => handleSort("name")}>
+                        Coin {sortConfig.key === "name" && (sortConfig.direction === "asc" ? "↑" : "↓")}
+                    </span>
+                    <span className="crypto_table_header__price" onClick={() => handleSort("priceUsd")}>
+                        Price {sortConfig.key === "priceUsd" && (sortConfig.direction === "asc" ? "↑" : "↓")}
+                    </span>
+                    <span className="crypto_table_header__change" onClick={() => handleSort("changePercent24Hr")}>
+                        24Hr % {sortConfig.key === "changePercent24Hr" && (sortConfig.direction === "asc" ? "↑" : "↓")}
+                    </span>
+                </li>
+                {sortedCryptoList.slice(0, visibleCount).map((crypto) => (
+                    <li
+                        className="crypto_table_item"
+                        key={crypto.id}
+                        onClick={() => {
+                            getHistory(crypto.id);
+                            window.scrollTo({ top: 0, behavior: "smooth" });
+                        }}
+                    >
+                        <ul className="crypto_table_item-rank">
+                            <li>{parseInt(crypto.rank)}</li> {/* Преобразование rank в число */}
+                        </ul>
                         <img
                             src={`https://assets.coincap.io/assets/icons/${crypto.symbol.toLowerCase()}@2x.png`}
                             alt="Crypto_img"
                         />
-                    <ul className="crypto_table_item-name">
-                        <li>{crypto.name}</li>
-                        <li className="symbol">{crypto.symbol}</li>
-                    </ul>
-                    <ul className="crypto_table_item__price">
-                        <li>$ {Math.round(crypto.priceUsd * 100) / 100}</li>
-                    </ul>
-                    <ul className="crypto_table_item__change" style={{color: crypto.changePercent24Hr >= 0 ? "#56e372" : "#e65c5c",}}>
-						<li>{crypto.changePercent24Hr > 0 ? `${'↗'}` : '↘'} {Math.round(crypto.changePercent24Hr * 100) / 100} &nbsp;%</li>
-					</ul>
-                </li>
-            ))}
-        </ul>
+                        <ul className="crypto_table_item-name">
+                            <li>{crypto.name}</li>
+                            <li className="symbol">{crypto.symbol}</li>
+                        </ul>
+                        <ul className="crypto_table_item__price">
+                            <li>$ {Math.round(crypto.priceUsd * 100) / 100}</li>
+                        </ul>
+                        <ul
+                            className="crypto_table_item__change"
+                            style={{
+                                color: crypto.changePercent24Hr >= 0 ? "#56e372" : "#e65c5c",
+                            }}
+                        >
+                            <li>
+                                {crypto.changePercent24Hr > 0 ? "↗" : "↘"}{" "}
+                                {Math.round(crypto.changePercent24Hr * 100) / 100} &nbsp;%
+                            </li>
+                        </ul>
+                    </li>
+                ))}
+            </ul>
+            {/* Кнопка для показа больше */}
+            {visibleCount < cryptoList.length && (
+                <button onClick={showMore} className="show_more_button">
+                    Show More
+                </button>
+            )}
+        </div>
     );
 }
 
--- src/component/LineChart.jsx
+++ src/component/LineChart.jsx
@@ -1,101 +1,3 @@
-// import React from 'react';
-// import {Line} from 'react-chartjs-2';
-// import {
-//     CategoryScale, Chart as ChartJS, Filler, Legend, LinearScale, LineElement, PointElement, Title, Tooltip,
-// } from 'chart.js';
-//
-// ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler, Legend);
-//
-// const OPTIONS = {
-//     responsive: true, maintainAspectRatio: false, // aspectRatio: 1|1,
-//     plugins: {
-//         legend: {
-//             display: false,
-//         }, filler: {
-//             propagate: true
-//         },
-//     }, scales: {
-//         y: {
-//             ticks: {
-//                 color: 'rgb(255,255,255)', font: {
-//                     size: 10,
-//                 },
-//             }, grid: {
-//                 display: true, color: 'rgba(255,255,255,0.05)',
-//             },
-//         }, x: {
-//             ticks: {
-//                 color: 'rgb(255,255,255)', font: {
-//                     size: 10,
-//                 },
-//             }, grid: {
-//                 display: true, color: 'rgba(255,255,255,0.05)',
-//             },
-//         },
-//     },
-// };
-//
-// const determineColors = (data, useTransparentColors) => {
-//     const colors = [];
-//     for (let i = 0; i < data.length - 1; i++) {
-//         if (data[i] < data[i + 1]) {
-//             colors.push(useTransparentColors ? 'rgba(0, 255, 0, 0.3)' : 'green'); // Зеленый для роста
-//         } else {
-//             colors.push(useTransparentColors ? 'rgba(255, 0, 0, 0.3)' : 'red'); // Красный для падения
-//         }
-//     }
-//     return colors;
-// };
-// export const LineChart = ({data, labels, selectedCrypto}) => {
-//     if (!selectedCrypto) {
-//         return null;
-//     }
-//
-//     const chartData = {
-//         labels: labels, datasets: [{
-//             label: selectedCrypto.name,
-//             data: data, // fill: true,
-//             borderColor: determineColors(data, false),
-//             backgroundColor: determineColors(data, true),
-//             borderWidth: 1.5,
-//             pointRadius: 2,
-//             tension: 0.4,
-//             fill: {
-//                 display: true, target: 'origin', above: determineColors(data, true),
-//             },
-//         },],
-//     };
-//
-//     const priceChange = data[data.length - 1] - data[0];
-//     const priceColor = priceChange > 0 ? 'green' : 'red';
-//     const arrowIcon = priceChange > 0 ? '↑' : '↓';
-//
-//     return (<div className="lineChart">
-//             <div className="header">
-//                 <div className="crypto_name">
-//                     <h3>
-//                         {selectedCrypto.name}({selectedCrypto.symbol})
-//                     </h3>
-//                 </div>
-//                 <div className="crypto_img">
-//                     <img
-//                         src={`https://assets.coincap.io/assets/icons/${selectedCrypto.symbol.toLowerCase()}@2x.png`}
-//                         alt="Crypto_img"
-//                     />
-//                 </div>
-//                     <div className="crypto_price">
-//                     <h3 style={{color: priceColor}}>
-//                         $ {Math.round(selectedCrypto.priceUsd * 100) / 100} {arrowIcon}
-//                     </h3>
-//                 </div>
-//             </div>
-//             <div className="chart-container">
-//                 <Line options={OPTIONS} data={chartData}/>
-//             </div>
-//         </div>);
-// };
-//
-// export default LineChart;
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import {
@@ -202,35 +104,63 @@ export const LineChart = ({ data, labels, selectedCrypto }) => {
     const priceColor = priceChange > 0 ? 'green' : 'red';
     const arrowIcon = priceChange > 0 ? '▲' : '▼';
 
+    function formatLargeNumber(number) {
+        const suffixes = ["", "k", "m", "b", "t"]; // Суффиксы для тысяч, миллионов и т.д.
+        const tier = Math.floor(Math.log10(Math.abs(number)) / 3); // Определяем порядок числа
+
+        if (tier === 0) return `$${number.toFixed(2)}`; // Для чисел менее 1000
+
+        const scaled = number / Math.pow(10, tier * 3); // Делим число на соответствующий порядок
+        const suffix = suffixes[tier]; // Получаем соответствующий суффикс
+        return `$${scaled.toFixed(2)}${suffix}`; // Возвращаем форматированное значение
+    }
+
+
     return (
         <div className="lineChart">
             <div className="header">
                 <div className="crypto_name">
                     <h3>
-                        {selectedCrypto.name}({selectedCrypto.symbol})
+                        #{selectedCrypto.rank} {selectedCrypto.name}({selectedCrypto.symbol})
                     </h3>
                 </div>
                 <div className="crypto_img">
+                    <a href={selectedCrypto.site} target="_blank">
+                        <img
+                            src={`https://assets.coincap.io/assets/icons/${selectedCrypto.symbol.toLowerCase()}@2x.png`}
+                            alt="Crypto_img"
+                        />
+                    </a>
+                </div>
+                <div className="crypto_price">
+                    <h3 style={{color: priceColor}}>
+                        {arrowIcon} $ {Math.round(selectedCrypto.priceUsd * 100) / 100}
+                    </h3>
+                </div>
+            </div>
+            <div className="header_info">
+                <div className="crypto_info">
+                    <p>
+                        Market Cap - {formatLargeNumber(selectedCrypto.marketCapUsd)}
+                    </p>
+                    <p>
+                        Volume (24Hr) - {formatLargeNumber(selectedCrypto.volumeUsd24Hr)}
+                    </p>
+                    {/*<p>*/}
+                    {/*    explorer - {selectedCrypto.explorer}*/}
+                    {/*</p>*/}
+                </div>
+            </div>
+
+            <div className="chart-container">
+                <Line options={OPTIONS} data={chartData}/>
+                <div className="chart-container_bg">
                     <img
                         src={`https://assets.coincap.io/assets/icons/${selectedCrypto.symbol.toLowerCase()}@2x.png`}
                         alt="Crypto_img"
                     />
                 </div>
-                <div className="crypto_price">
-                    <h3 style={{ color: priceColor }}>
-                        {arrowIcon} $ {Math.round(selectedCrypto.priceUsd * 100) / 100}
-                    </h3>
-                </div>
             </div>
-            <div className="chart-container">
-                <Line options={OPTIONS} data={chartData} />
-            <div className="chart-container_bg">
-            <img
-                        src={`https://assets.coincap.io/assets/icons/${selectedCrypto.symbol.toLowerCase()}@2x.png`}
-                        alt="Crypto_img"
-                    />
-            </div>
-            </div>           
         </div>
     );
 };
